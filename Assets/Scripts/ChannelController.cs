a35e9aba9ed750932f1c068fca9efc5e1f2bc485
Bryan Tai <redhatpieman@gmail.com>
2018-01-27 15:36:59 -0800

    Radio channel audio plays when dial is on specific stations

--- Assets/Scripts/ChannelController.cs
+++ Assets/Scripts/ChannelController.cs
@@ -8,30 +8,47 @@ public class ChannelController : MonoBehaviour {
     private AudioSource[] audioSources;
 
     private AudioClip[] radioSFX;
+    private AudioClip[][] policeClips;
     private AudioClip[] weedmanClips;
 
     private const string VO_PATH = "Sounds/VO";
     private const float VOL_DELTA = 0.1f;
+    private const int POLICE_CHANNELS = 3;
+
+    private int lastPlayingStation = 0;
 
     // Use this for initialization
     void Start () {
         audioSources = GetComponents<AudioSource>();
         RadioTuning = audioSources[0];
         radioChannels = new AudioSource[audioSources.Length - 1];
-        for (int c = 1; c < audioSources.Length; c++)
+
+        for (int c = 0; c < audioSources.Length - 1; c++)
         {
-            radioChannels[c - 1] = audioSources[c];
+            radioChannels[c] = audioSources[c+1];
         }
         
 
         radioSFX = Resources.LoadAll<AudioClip>("Sounds/SFX");
         weedmanClips = Resources.LoadAll<AudioClip>(VO_PATH + "Weedman");
+        policeClips = new AudioClip[3][];
+        for (int p = 0; p < POLICE_CHANNELS; p++)
+        {
+            string path = VO_PATH + "/Channel" + (p + 1);
+            policeClips[p] = Resources.LoadAll<AudioClip>(path);
+
+            radioChannels[p].clip = policeClips[p][0];
+            radioChannels[p].volume = 0;
+            radioChannels[p].loop = true;
+            radioChannels[p].Play();
+        }
 
         RadioTuning.clip = radioSFX[1];
         RadioTuning.volume = 0;
         RadioTuning.loop = true;
         RadioTuning.Play();
-	}
+
+    }
 	
     //returns 0 - 2 for police channels
     //        3 for weed
@@ -63,11 +80,6 @@ public class ChannelController : MonoBehaviour {
             }
         }else
         {
-            if ( currentChannel < 0) {
-                
-            }
-                //TODO: Play staticSound OR Random channel
-
             RadioTuning.volume -= VOL_DELTA;
         }
 
@@ -85,6 +97,18 @@ public class ChannelController : MonoBehaviour {
                 radioChannels[currentChannel + 1].volume -= VOL_DELTA;
             }
         }
+        else
+        {
+            //Static, make the channels quieter
+            foreach (AudioSource radioChannel in radioChannels)
+            {
+                if (radioChannel.volume > 0)
+                {
+                    radioChannel.volume -= VOL_DELTA;
+                }
+            }
+        }
+
         if (currentChannel == 3)
         {
             //TODO Weed Channel, let Player interact with Map
