951acad05428bdace0e62a7c054494b0041627cd
Bryan Tai <redhatpieman@gmail.com>
2018-01-27 11:29:06 -0800

    Moved audio files to Resources, started Channels

---
+++ Assets/Scripts/ChannelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChannelController : MonoBehaviour {
+    public AudioSource channel_1;
+    public AudioSource channel_2;
+    public AudioSource channel_weed;
+
+    private AudioClip[] radioSFX;
+
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
