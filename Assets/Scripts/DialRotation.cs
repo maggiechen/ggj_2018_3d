3627f46dd60a543c91c9904ef11833024509f893
MrigankaArya <mrig.arya@gmail.com>
2018-01-27 10:25:57 -0800

    slider stuff

--- Assets/Scripts/DialRotation.cs
+++ Assets/Scripts/DialRotation.cs
@@ -15,17 +15,26 @@ public class DialRotation : MonoBehaviour {
         return angle;
     }
 
+    void MoveSlider(int dir) {
+        GameObject slider = GameObject.Find ("Slider");
+        slider.transform.Translate (Vector3.right * dir* RotationSpeed/25);
+
+    }
+
 	// Update is called once per frame
 	void Update () {
         float angle = WrapAngle(gameObject.transform.localRotation.eulerAngles.z);
         if (Input.GetKey(KeyCode.LeftArrow) && angle < 90f) 
         {
+            MoveSlider (-1);
             gameObject.transform.Rotate(0f, 0f, RotationSpeed);
         }
         else if (Input.GetKey(KeyCode.RightArrow) && angle > -90f)
         {
+            MoveSlider (1);
             gameObject.transform.Rotate(0f, 0f, -RotationSpeed);
         }
 
 	}
+        
 }
