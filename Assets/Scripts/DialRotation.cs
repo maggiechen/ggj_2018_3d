558ab8301293995e565fcb333139d9a9cd4b9476
Jacob Madrid <madridjacobr@gmail.com>
2018-01-27 00:03:08 -0800

    Rotation speed now changeable from inspector

--- Assets/Scripts/DialRotation.cs
+++ Assets/Scripts/DialRotation.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class DialRotation : MonoBehaviour {
 
+    public float DeltaPerFrame = 0.5f;
 
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            gameObject.transform.Rotate(0f, 0f, 0.5f);
+            gameObject.transform.Rotate(0f, 0f, DeltaPerFrame);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            gameObject.transform.Rotate(0f, 0f, -0.5f);
+            gameObject.transform.Rotate(0f, 0f, -DeltaPerFrame);
         }
 
 	}
