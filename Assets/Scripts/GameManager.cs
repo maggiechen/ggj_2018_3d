7f418ac8bfe26ae1a4caa8d585978e161219e309
Maggie Chen <maggieyudanchen@gmail.com>
2018-01-26 23:17:57 -0800

    add game state

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager {
+    private static GameManager instance;
+    public GameStateMachine gameStateMachine;
+    private GameManager()
+    {
+        // hooboy a game manager
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateMachine
+{
+    private Dictionary<StateType, GameState> states = new Dictionary<StateType, GameState>()
+    {
+        {StateType.Intro, new IntroState()},
+        {StateType.Playing, new PlayingState()},
+        {StateType.Paused, new PausedState()},
+        {StateType.BadEnd, new BadEndState()},
+        {StateType.GoodEnd, new GoodEndState()}
+    };
+
+    private StateType currentState = StateType.Intro;
+    public StateType previousState = StateType.Intro;
+
+    public bool pauseRequested;
+    public bool bad;
+
+    public void Pause()
+    {
+        pauseRequested = true;
+        AdvanceState();
+    }
+
+    public void AdvanceState()
+    {
+        previousState = currentState;
+        currentState = states[currentState].GetNextState(this);
+    }
+}
+
+public enum StateType
+{
+    Intro,
+    Playing,
+    Paused,
+    BadEnd,
+    GoodEnd
+}
+
+public interface GameState
+{
+    StateType GetNextState(GameStateMachine sm);
+}
+
+public class IntroState : GameState
+{
+
+    public StateType GetNextState(GameStateMachine sm)
+    {
+        return StateType.Playing;
+    }
+}
+
+public class PlayingState : GameState
+{
+    public StateType GetNextState(GameStateMachine sm)
+    {
+        if (sm.pauseRequested)
+        {
+            return StateType.Paused;
+        }
+        else if (sm.bad)
+        {
+            return StateType.BadEnd;
+        }
+        else
+        {
+            return StateType.GoodEnd;
+        }
+    }
+}
+
+public class PausedState : GameState
+{
+    public StateType GetNextState(GameStateMachine sm)
+    {
+        sm.pauseRequested = false;
+        return sm.previousState;
+    }
+}
+
+public class BadEndState : GameState
+{
+    public StateType GetNextState(GameStateMachine sm)
+    {
+        return StateType.Intro;
+    }
+}
+
+public class GoodEndState : GameState
+{
+    public StateType GetNextState(GameStateMachine sm)
+    {
+        return StateType.Intro;
+    }
+}
