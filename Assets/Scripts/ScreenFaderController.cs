7721137d01acf30cadb8711409f7ad8c762cdd66
Maggie Chen <maggieyudanchen@gmail.com>
2018-01-27 11:18:57 -0800

    screenfader and move timer back to radio

--- Assets/Scripts/ScreenFaderController.cs
+++ Assets/Scripts/ScreenFaderController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScreenFaderController : MonoBehaviour {
-
+    public CanvasGroup canvasGroup;
+    public float fadeRate;
 	// Use this for initialization
 	void Start () {
-		
+        GameManager gameInstance = GameManager.Instance;
+        if (gameInstance.gameStateMachine.currentState == StateType.Intro)
+        {
+            canvasGroup.alpha = 1;
+            StartCoroutine(FadeToClear());
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    IEnumerator FadeToClear()
+    {
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= fadeRate;
+            yield return null;
+        }
+    }
 }
