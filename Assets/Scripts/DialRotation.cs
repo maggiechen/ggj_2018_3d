fc85b23cdad79f6f6691d306dc9905ecd24673d2
Jacob Madrid <madridjacobr@gmail.com>
2018-01-27 00:01:59 -0800

    Rotation with left and arrow keys

---
+++ Assets/Scripts/DialRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialRotation : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.Rotate(0f, 0f, 0.5f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Rotate(0f, 0f, -0.5f);
+        }
+
+	}
+}
