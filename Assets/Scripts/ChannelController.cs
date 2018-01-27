a5428c7aaa9e733084a83c1742b5abc6d9e440f4
Bryan Tai <redhatpieman@gmail.com>
2018-01-27 12:07:00 -0800

    Added static sfx to dial

--- Assets/Scripts/ChannelController.cs
+++ Assets/Scripts/ChannelController.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class ChannelController : MonoBehaviour {
     public AudioSource channel_1;
     public AudioSource channel_2;
+    public AudioSource channel_3;
     public AudioSource channel_weed;
+    public AudioSource Static;
 
     private AudioClip[] radioSFX;
 
 	// Use this for initialization
 	void Start () {
-        
-		
+        radioSFX = Resources.LoadAll<AudioClip>("Sounds/SFX");
+
+        Static.clip = radioSFX[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        bool dialMoving = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow);
+        if (dialMoving)
+        {
+            if (!Static.isPlaying)
+            {
+                Static.Play();
+            }
+        }else
+        {
+            //TODO check if on a station
+            Static.Stop();
+        }
 	}
+
+
 }
