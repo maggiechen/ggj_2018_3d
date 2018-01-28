b45ca865b959c0a93a8ec8cb4e0117acceb9ac22
MrigankaArya <mrig.arya@gmail.com>
2018-01-28 14:49:13 -0800

    You can now turn the radio on and make it slightly uncomfortable.

--- Assets/Scripts/OnOffController.cs
+++ Assets/Scripts/OnOffController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class OnOffController : MonoBehaviour {
     ChannelController channels;
-
+    bool isOn;
+    MeshRenderer spriteCan;
+    public Material mat;
     void Start()
     {
         channels = FindObjectOfType<ChannelController>();
+        isOn = false;
+        spriteCan = GetComponent<MeshRenderer>();
     }
 
     private void OnMouseDown()
     {
+        if (!isOn) {
+            Material[] mats = spriteCan.materials;
+            mats[0] = mat;
+            spriteCan.materials = mats;
+        }
+
         if (GameManager.Instance.gameStateMachine.currentState == StateType.Intro)
         {
             return;
