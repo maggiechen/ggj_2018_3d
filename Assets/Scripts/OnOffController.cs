a775af8c622438532faa3781724785359c17d4e4
Maggie Chen <maggieyudanchen@gmail.com>
2018-01-27 17:06:22 -0800

    off button and state changes

---
+++ Assets/Scripts/OnOffController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffController : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        if (GameManager.Instance.gameStateMachine.currentState == StateType.Intro)
+        {
+            return;
+        }
+
+        if (GameManager.Instance.gameStateMachine.currentState == StateType.Off)
+        {
+            GameManager.Instance.gameStateMachine.AdvanceState();
+        } else
+        {
+            Debug.Log("Performing Quit");
+            Application.Quit();
+        }
+    }
+}
