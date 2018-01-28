7e8fedcbefc409acd084313fd6c7e24a48147e90
Maggie Chen <maggieyudanchen@gmail.com>
2018-01-28 12:46:12 -0800

    pretty hover map!

--- Assets/Scripts/AreaTracker.cs
+++ Assets/Scripts/AreaTracker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,15 @@ public class AreaTracker : MonoBehaviour {
         {
             int policeCount = GameManager.Instance.gameStateMachine.locations[i];
             policeCounts[i].text = policeCount.ToString();
+        }
+    }
+
+    internal void DisableAllAreas()
+    {
+        for (int i = 0; i < areas.Count; i++)
+        {
+            areas[i].SetDefaultMaterial();
+            areas[i].selected = false;
         }
     }
 }
